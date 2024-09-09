26bbc6414b8d55ce543a2cfec75c3d6daeea039d
richangchangri <111625443+richangchangri@users.noreply.github.com>
2024-09-09 09:38:35 -0400

    asig3

---
+++ assignments/Assignment 3/Library/Bee/artifacts/WebGL/il2cppOutput/cpp/Assembly-CSharp_CodeGen.c
@@ -0,0 +1,56 @@
+﻿#include "pch-c.h"
+#ifndef _MSC_VER
+# include <alloca.h>
+#else
+# include <malloc.h>
+#endif
+
+
+#include "codegen/il2cpp-codegen-metadata.h"
+
+
+
+
+
+extern void ballscript_Start_m01CB30673D62C24355B4CEF5F965D34AD5F384D9 (void);
+extern void ballscript_Update_mD1BB939D268B12179BC9D0587B59BF48C19F2BB0 (void);
+extern void ballscript__ctor_mC224E35C46B6916DB0B426CE25EFF079E0D80CD6 (void);
+extern void UnitySourceGeneratedAssemblyMonoScriptTypes_v1_Get_mBEB95BEB954BB63E9710BBC7AD5E78C4CB0A0033 (void);
+extern void UnitySourceGeneratedAssemblyMonoScriptTypes_v1__ctor_mE70FB23ACC1EA12ABC948AA22C2E78B2D0AA39B1 (void);
+static Il2CppMethodPointer s_methodPointers[5] = 
+{
+	ballscript_Start_m01CB30673D62C24355B4CEF5F965D34AD5F384D9,
+	ballscript_Update_mD1BB939D268B12179BC9D0587B59BF48C19F2BB0,
+	ballscript__ctor_mC224E35C46B6916DB0B426CE25EFF079E0D80CD6,
+	UnitySourceGeneratedAssemblyMonoScriptTypes_v1_Get_mBEB95BEB954BB63E9710BBC7AD5E78C4CB0A0033,
+	UnitySourceGeneratedAssemblyMonoScriptTypes_v1__ctor_mE70FB23ACC1EA12ABC948AA22C2E78B2D0AA39B1,
+};
+static const int32_t s_InvokerIndices[5] = 
+{
+	1100,
+	1100,
+	1100,
+	2125,
+	1100,
+};
+IL2CPP_EXTERN_C const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule;
+const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule = 
+{
+	"Assembly-CSharp.dll",
+	5,
+	s_methodPointers,
+	0,
+	NULL,
+	s_InvokerIndices,
+	0,
+	NULL,
+	0,
+	NULL,
+	0,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+};
